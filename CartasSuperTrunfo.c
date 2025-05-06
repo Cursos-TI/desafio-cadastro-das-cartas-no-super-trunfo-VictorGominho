71c276e0d2c79d99b69ee79d1b1daba04ff6908b
Victor Gominho <89556454+VictorGominho@users.noreply.github.com>
2025-05-06 19:47:09 +0000

    Nível Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,30 +4,24 @@
 
 int main() {
      char estado1, codigo1[4], nome1[50], estado2, codigo2[4], nome2[50];
-     int populacao1, ponTuristicos1, populacao2, ponTuristicos2;
-     float area1, pib1, area2, pib2, densoPop1, densoPop2, pibCapita1, pibCapita2;
+     int ponTuristicos1, ponTuristicos2;
+     unsigned long int populacao1, populacao2;
+     float area1, pib1, area2, pib2, densoPop1, densoPop2, pibCapita1, pibCapita2, superPoder1, superPoder2;
+     int resultadoPop, resultadoArea, resultadoPib, resultadoPont, resultadoDensPop, resultadoPibCap, resultadoSuper;
 
      printf("Digite as seguintes informações da carta 1: \n\n");
 
      printf("Nome da carta, sem acentos: ");
-     scanf("%[^\n]s", nome1); // Aparentemente o C ODEIA strings compostas e acentos.
+     scanf("%[^\n]s", nome1); // Habilita strings compostas
 
-     /*
-     Facilmente a parte do código que me quebrou. Eu não faço idéia de por que é tão difícil ler uma string composta em C, mas tudo que podia ter dado errado pra essa linha de código não funcionar, deu. Se eu implementar essa solução em outro código e falhar, posso muito bem desistir dessa linguagem de programação. E eu ainda não consegui fazer funcionar com palavras com acento.
-     */
-       
      printf("Estado (primeira letra): ");
      scanf(" %c", &estado1);
 
      printf("Código (letra do estado + número): ");
      scanf("%s", codigo1);
 
-     /*
-     Eu tentei fazer com que o código montasse o código da carta sozinho, invés de depender do usuário, mas sem uma estrutura de repetição, é complicado. Refazer mais tarde em um outro exercício?
-     */
-
      printf("População: ");
-     scanf("%d", &populacao1);
+     scanf("%lu", &populacao1);
 
      printf("Área em Km²: ");
      scanf("%f", &area1);
@@ -57,7 +51,7 @@ int main() {
      scanf("%s", codigo2);
 
      printf("População: ");
-     scanf("%d", &populacao2);
+     scanf("%lu", &populacao2);
 
      printf("Área em Km²: ");
      scanf("%f", &area2);
@@ -73,20 +67,49 @@ int main() {
 
      pibCapita2 = (pib2 * 1000000000) / populacao2;
 
+     //Calculando o Super Poder de ambas as cartas
+     superPoder1 = populacao1 + area1 + pib1 + pibCapita1 + ponTuristicos1 - densoPop1;
+     superPoder2 = populacao2 + area2 + pib2 + pibCapita2 + ponTuristicos2 - densoPop2;
+     /*
+     printf("Super poder 1: %.2f\n", superPoder1);
+     printf("Super poder 2: %.2f\n", superPoder2);
+     */
+    
      //Imprimindo resultados:
      printf("\nCarta 1:\n");
      printf("Estado: %c \n", estado1);
-     printf("Código: %s \nNome: %s \nPopulação: %d", codigo1, nome1, populacao1);
+     printf("Código: %s \nNome: %s \nPopulação: %lu", codigo1, nome1, populacao1);
      printf("\nÁrea: %.2fKm² \nPIB: R$ %.2f bilhões \nPontos turísticos: %d", area1, pib1, ponTuristicos1);
      printf("\nDensidade Populacional: %.2f hab/km²", densoPop1);
      printf("\nPIB per Capita: R$ %.2f", pibCapita1);
 
      printf("\n\nCarta 2:\n");
      printf("Estado: %c \n", estado2);
-     printf("Código: %s \nNome: %s \nPopulação: %d", codigo2, nome2, populacao2);
+     printf("Código: %s \nNome: %s \nPopulação: %lu", codigo2, nome2, populacao2);
      printf("\nÁrea: %.2fKm² \nPIB: R$ %.2f bilhões \nPontos turísticos: %d", area2, pib2, ponTuristicos2);
      printf("\nDensidade Populacional: %.2f hab/km²", densoPop2);
      printf("\nPIB per Capita: R$ %.2f", pibCapita2);
+
+     //Comparando as cartas
+     resultadoPop = populacao1 > populacao2;
+     resultadoArea = area1 > area2;
+     resultadoPib = pib1 > pib2;
+     resultadoPont = ponTuristicos1 > ponTuristicos2;
+     resultadoDensPop = densoPop1 < densoPop2;
+     resultadoPibCap = pibCapita1 > pibCapita2;
+     resultadoSuper = superPoder1 > superPoder2;
+
+     printf("\n\nComparação de Cartas:\n\n");
+     /*
+     Essa seção do código claramente precisa de uma condição if pra mostrar a carta certa, mas o exercício não permite, então os resultados são estáticos. Existem outros métodos, mas estou tentando usar somente o que foi ensinado até agora.
+     */
+     printf("População: Carta 1 venceu (%d)\n", resultadoPop);
+     printf("Área: Carta 1 venceu (%d)\n", resultadoArea);
+     printf("PIB: Carta 1 venceu (%d)\n", resultadoPib);
+     printf("Pontos Turísticos: Carta 1 venceu (%d)\n", resultadoPont);
+     printf("Densidade Populacional: Carta 2 venceu (%d)\n", resultadoDensPop);
+     printf("PIB per Capita: Carta 1 venceu (%d)\n", resultadoPibCap);
+     printf("Super Poder: Carta 1 venceu (%d)\n", resultadoSuper);
 
      return 0;
 }
